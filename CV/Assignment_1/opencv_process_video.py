a39bc6aa15ce3c8983ce3f202c32a05e65e8d1fa
Thibeau Wouters <thibeauwouters@gmail.com>
2023-02-28 22:32:16 +0100

    CV update

--- CV/Assignment_1/opencv_process_video.py
+++ CV/Assignment_1/opencv_process_video.py
@@ -33,34 +33,101 @@ def main(input_video_file: str, output_video_file: str) -> None:
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # saving output video as .mp4
     out = cv2.VideoWriter(output_video_file, fourcc, fps, (frame_width, frame_height))
 
-
     # Specify hyperparameters (for during working on frames)
     show = True  # show the frame as well
-    max_time = 5000  # max time that we are going to process
-    # Make sure max_time is at most 60000, or 1 minute
+    min_time = 12000
+    max_time = 16000  # max time that we are going to process
+    # Make sure max_time is at most 60000, i.e. 1 minute
     max_time = min(60000, max_time)
-    print(f"Max time is {max_time/1000} seconds.")
+    print(f"Procesing frames between {min_time/1000} seconds and {max_time/1000} seconds.")
+    font = cv2.FONT_HERSHEY_PLAIN
+    subtitle = ""
 
     while cap.isOpened():
-        # Press a certain button to go to the next frame
-
         ret, frame = cap.read()
-        font = cv2.FONT_HERSHEY_PLAIN
 
         if ret:
-            if between(cap, 0, 10000):
-                pass
-                #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            # Add subtitle
-            subtitle = "AAAAAAAA"
-            cv2.putText(frame, subtitle, (int(0.5*frame_width), int(0.1*frame_height)), font, 2, (0, 0, 255), 2, cv2.LINE_4)
+            # If before min time, we don't handle those frames (used when constructing video)
+            if between(cap, 0, min_time):
+                continue
 
-            # write frame that you processed to output
+            "0 -- 4 seconds"
+            if between(cap, 0, 1000) or between(cap, 2000, 3000):
+                # Convert to greyscale, but make sure we still have three channels (copy one channel to others)
+                frame[:, :, 1] = frame[:, :, 0]
+                frame[:, :, 2] = frame[:, :, 0]
+                subtitle = "Switch between greyscale and color"
+
+            "4 -- 8 seconds: Gaussian blurring"
+            if between(cap, 4000, 5000):
+                kernel = (31, 31)
+                sigma = 0
+                frame = cv2.GaussianBlur(frame, kernel, sigma)
+
+                subtitle = f"Gaussian filter: kernel {kernel}, sigma: {sigma}"
+
+            if between(cap, 5000, 6000):
+                kernel = (51, 51)
+                sigma = 0
+                frame = cv2.GaussianBlur(frame, kernel, sigma)
+
+                subtitle = f"Gaussian filter: kernel {kernel}, sigma: {sigma}"
+
+            if between(cap, 6000, 7000):
+                kernel = (51, 51)
+                sigma = 10
+                frame = cv2.GaussianBlur(frame, kernel, sigma)
+
+                subtitle = f"Gaussian filter: kernel {kernel}, sigma: {sigma}"
+
+            if between(cap, 7000, 8000):
+                kernel = (51, 51)
+                sigma = 20
+                frame = cv2.GaussianBlur(frame, kernel, sigma)
+
+                subtitle = f"Gaussian filter: kernel {kernel}, sigma: {sigma}"
+
+            "8 -- 12 seconds: bilateral filter"
+            if between(cap, 8000, 10000):
+                d = 15
+                sigma_color = 10
+                sigma_space = 10
+                frame = cv2.bilateralFilter(frame, d, sigma_color, sigma_space)
+
+                subtitle = f"Bilateral filter: d: {d}, sigma color {sigma_color}, sigma space: {sigma_space}"
+
+            if between(cap, 10000, 12000):
+                d = 15
+                sigma_color = 50
+                sigma_space = 50
+                frame = cv2.bilateralFilter(frame, d, sigma_color, sigma_space)
+
+                subtitle = f"Bilateral filter: d: {d}, sigma color {sigma_color}, sigma space: {sigma_space}"
+
+            "12 -- 16 seconds: simple grabbing by thresholding"
+            if between(cap, 12000, 16000):
+                # Convert to HSV space
+                hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                # Blur
+                hsv = cv2.GaussianBlur(hsv, (13, 13), 0)
+                # Specify threshold ranges
+                lower = np.array([0, 150, 0])
+                upper = np.array([179, 255, 255])
+                mask = cv2.inRange(hsv, lower, upper)
+                # # Bitwise-AND mask and original image
+                white = 255*np.ones_like(hsv)
+                frame = cv2.bitwise_and(white, white, mask=mask)
+                subtitle = f"Grabbing (threshold in HSV space)"
+
+            # Write subtitle, with a timestamp, and save to write frame to output
+            timestamp = '{:.3f}'.format(get_time(cap)/1000)
+            cv2.putText(frame, subtitle + f" ({timestamp} s)", (int(0.1*frame_width), int(0.1*frame_height)), font, 2, (0, 0, 255), 2, cv2.LINE_4)
             out.write(frame)
 
             # (optional) display the resulting frame
             if show:
+                cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
                 cv2.imshow('Frame', frame)
                 cv2.waitKey(1)
 
@@ -79,7 +146,6 @@ def main(input_video_file: str, output_video_file: str) -> None:
     out.release()
     # Closes all the frames
     cv2.destroyAllWindows()
-    print("Done")
 
 
 if __name__ == '__main__':
--- CV/Demo/demo.py
+++ CV/Demo/demo.py
@@ -20,19 +20,181 @@ def numpy_example():
     plt.imshow(noisy, cmap="gray")
     plt.show()
 
-def cv_example():
-    image = cv2.imread("nighthawks.jpg")
-    print(image[:, :, 0])
-    #print(image[:, :, 1])
-    #print(image[:, :, 2])
-    plt.figure()
-    plt.imshow(image)
-    plt.show()
 
-    # Smoothen it with a Gaussian kernel
-    blur = cv2.GaussianBlur(image, (5, 5    ), 0)
-    plt.figure()
-    plt.imshow(blur)
-    plt.show()
+def bilateral_filter_example():
 
-cv_example()
+    # Load original image
+    image = cv2.imread("apple.jpeg")
+    # Edit it as you desire
+    d = 10
+    sigma_color = 50
+    sigma_space = 50
+    image = cv2.bilateralFilter(image, d, sigma_color, sigma_space)
+    # Show the output
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+
+def nothing(x):
+    pass
+
+def sliders_HSV(filename):
+    # Load in image
+    image = cv2.imread(filename)
+
+    # Create a window
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+
+    # create trackbars for color change
+    cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
+    cv2.createTrackbar('SMin','image',0,255,nothing)
+    cv2.createTrackbar('VMin','image',0,255,nothing)
+    cv2.createTrackbar('HMax','image',0,179,nothing)
+    cv2.createTrackbar('SMax','image',0,255,nothing)
+    cv2.createTrackbar('VMax','image',0,255,nothing)
+
+    # Set default value for MAX HSV trackbars.
+    cv2.setTrackbarPos('HMax', 'image', 179)
+    cv2.setTrackbarPos('SMax', 'image', 255)
+    cv2.setTrackbarPos('VMax', 'image', 255)
+
+    # Initialize to check if HSV min/max value changes
+    hMin = sMin = vMin = hMax = sMax = vMax = 0
+    phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+    output = image
+    wait_time = 33
+
+    while(1):
+
+        # get current positions of all trackbars
+        hMin = cv2.getTrackbarPos('HMin','image')
+        sMin = cv2.getTrackbarPos('SMin','image')
+        vMin = cv2.getTrackbarPos('VMin','image')
+
+        hMax = cv2.getTrackbarPos('HMax','image')
+        sMax = cv2.getTrackbarPos('SMax','image')
+        vMax = cv2.getTrackbarPos('VMax','image')
+
+        # Set minimum and max HSV values to display
+        lower = np.array([hMin, sMin, vMin])
+        upper = np.array([hMax, sMax, vMax])
+
+        # Create HSV Image and threshold into a range.
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower, upper)
+        output = cv2.bitwise_and(image,image, mask= mask)
+
+        # Print if there is a change in HSV value
+        if( (phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax) ):
+            print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (hMin , sMin , vMin, hMax, sMax , vMax), end='\r')
+            phMin = hMin
+            psMin = sMin
+            pvMin = vMin
+            phMax = hMax
+            psMax = sMax
+            pvMax = vMax
+
+        # Display output image
+        im = cv2.resize(output, (540, 270))  # Resize image
+        cv2.imshow('image', im)
+
+        # Wait longer to prevent freeze for videos.
+        if cv2.waitKey(wait_time) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+def grabbing():
+    # Load original image
+    image = cv2.imread("apple.jpeg")
+    # Show the output
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower = np.array([155, 25, 0])
+    upper = np.array([179, 255, 255])
+    mask = cv2.inRange(image, lower, upper)
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+
+
+def sliders_RGB(filename):
+    # Load in image
+    image = cv2.imread(filename)
+
+    # Create a window
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+
+    # create trackbars for color change
+    cv2.createTrackbar('BMin', 'image', 0, 255, nothing)
+    cv2.createTrackbar('GMin', 'image', 0, 255, nothing)
+    cv2.createTrackbar('RMin', 'image', 0, 255, nothing)
+    cv2.createTrackbar('BMax', 'image', 0, 255, nothing)
+    cv2.createTrackbar('GMax', 'image', 0, 255, nothing)
+    cv2.createTrackbar('RMax', 'image', 0, 255, nothing)
+
+    # Set default value for MAX HSV trackbars.
+    cv2.setTrackbarPos('BMax', 'image', 255)
+    cv2.setTrackbarPos('GMax', 'image', 255)
+    cv2.setTrackbarPos('RMax', 'image', 255)
+
+    # Initialize to check if HSV min/max value changes
+    BMin = GMin = RMin = BMax = GMax = RMax = 0
+    pBMin = pGMin = pRMin = pBMax = pGMax = pRMax = 0
+
+    output = image
+    wait_time = 33
+
+    while(1):
+
+        # get current positions of all trackbars
+        BMin = cv2.getTrackbarPos('BMin', 'image')
+        GMin = cv2.getTrackbarPos('GMin', 'image')
+        RMin = cv2.getTrackbarPos('RMin', 'image')
+
+        BMax = cv2.getTrackbarPos('BMax', 'image')
+        GMax = cv2.getTrackbarPos('GMax', 'image')
+        RMax = cv2.getTrackbarPos('RMax', 'image')
+
+        # Set minimum and max HSV values to display
+        lower = np.array([BMin, GMin, RMin])
+        upper = np.array([BMax, GMax, RMax])
+
+        # Create HSV Image and threshold into a range.
+        #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(image, lower, upper)
+        output = cv2.bitwise_and(image, image, mask=mask)
+
+        # Print if there is a change in HSV value
+        if( (pBMin != BMin) | (pGMin != GMin) | (pRMin != RMin) | (pBMax != BMax) | (pGMax != GMax) | (pRMax != RMax) ):
+            print("(BMin = %d , GMin = %d, RMin = %d), (BMax = %d , GMax = %d, RMax = %d)" % (BMin, GMin, RMin, BMax, GMax, RMax), end='\r')
+            pBMin = BMin
+            pGMin = GMin
+            pRMin = RMin
+            pBMax = BMax
+            pGMax = GMax
+            pRMax = RMax
+
+        # Display output image
+        # output = cv2.resize(output, (540, 270))  # Resize image
+        cv2.imshow('image', output)
+
+        # Wait longer to prevent freeze for videos.
+        if cv2.waitKey(wait_time) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+def grabbing():
+    # Load original image
+    image = cv2.imread("apple.jpeg")
+    # Show the output
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower = np.array([155, 25, 0])
+    upper = np.array([179, 255, 255])
+    image = cv2.inRange(image, lower, upper)
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+
+
+# grabbing()
+sliders_HSV('onion2.jpg')
+# sliders_RGB('frame.jpg')
